722ad6fa41b115796609bbad5a53f7d593f868be
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-08-21 02:00:33 +0200

    Updated make_predict_azure_app.sh filed

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://myflaskmlwebappy.azurewebsites.net:$PORT/predict 
+     -X POST https://myazurepipelinewebapp.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
